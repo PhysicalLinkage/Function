7da7454704bcbcb472ce8a3770df9a839421a107
PhysicalLinkage <physical-linkage@icloud.com>
2019-11-25 19:32:29 +0000

    init

---
+++ include/either.hpp
@@ -0,0 +1,75 @@
+#ifndef LATTE_EITHER_H
+#define LATTE_EITHER_H
+
+#include <utility>
+
+namespace Latte {
+
+template<class L, class R>
+class Either {
+
+    template<class LT> struct Left_  { const LT value; };
+    template<class RT> struct Right_ { const RT value; };
+
+    const bool is_left_;
+
+    union { const Left_<L> left_; const Right_<R> right_; };
+
+    const L& left()  const { return left_.value; }
+    const R& right() const { return right_.value; }
+
+    Either(const Left_<L>& value) : is_left_{true},  left_{value} {}
+    Either(const Right_<R>& value) : is_left_{false}, right_{value} {}
+
+    Either(Left_<L>&& value) : is_left_{true},  left_{std::move(value)} {}
+    Either(Right_<R>&& value) : is_left_{false}, right_{std::move(value)} {}
+
+    public: static auto Left(const L& value)  { return Either<L, R>{Left_<L>{value}}; }
+    public: static auto Right(const R& value) { return Either<L, R>{Right_<R>{value}}; }
+
+    public: static auto Left(L&& value)  { return Either<L, R>{Left_<L>{value}}; }
+    public: static auto Right(R&& value) { return Either<L, R>{Right_<R>{value}}; }
+
+    public: ~Either() {
+        if (is_left()) {
+            left().~L();
+        } else {
+            right().~R();
+        }
+    }
+
+    public: const bool is_left()  const { return is_left_; }
+    public: const bool is_right() const { return !is_left(); }
+
+    public: Either(const Either& other) : is_left_{other.is_left()} {
+        if (other.is_left()) {
+            new (const_cast<L*>(&left_.value))L{other.left()};
+        } else {
+            new (const_cast<R*>(&right_.value))R{other.right()};
+        }
+    }
+
+    public: template<class F> auto fmap(const F& f) const {
+        using NextEither = Either<L, decltype(f(right()))>;
+        return is_left() ? NextEither::Left(left()) : NextEither::Right(f(right()));
+    }
+
+    public: template<class F> auto bind(const F& f) const {
+        using NextEither = decltype(f(right()));
+        return is_left() ? NextEither::Left(left()) : f(right());
+    }
+
+    public: template<class LF, class RF> auto merge(const LF& lf, const RF& rf) const {
+        return is_left() ? lf(left()): rf(right());
+    }
+
+    public: auto reverse() const {
+        using NextEither = Either<R, L>;
+        return is_left() ? NextEither::Right(left()) : NextEither::Left(right());
+    }
+};
+
+}
+
+#endif
+
---
+++ test/either.cpp
@@ -0,0 +1,84 @@
+#include <either.hpp>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace Latte;
+
+int main() {
+
+    const auto id = [](const auto x) { return x; };
+    
+    const auto log = [](const std::string& x) { std::cout << "log : " << x << "\n"; return x; };
+
+    const auto log_int = [log](const int x) { std::cout << "log : " << std::to_string(x) << "\n"; return x; };
+
+    const auto add = [](const auto a) { return [a](const auto b) { return a + b; }; };
+
+    const auto to_string = [](const auto x) { return "success : " + std::to_string(x); };
+    
+    const auto error = [](const auto x) { return "error : " + x; };
+
+    const auto result = [](const auto x) { std::cout << "result : " + x + ".\n"; };
+
+    const auto start = [](const auto x) { std::cout << std::string("--------") + x + "--------\n"; };
+
+    const auto is_range = [](const size_t min, const size_t value, const size_t max) {
+        return (min <= value && value < max);
+    };
+
+    const auto get = [is_range](const std::vector<int>& vector, const size_t index) {
+        using ErrorOrInt = Either<std::string, int>;
+        return is_range(0, index, vector.size()) ?
+            ErrorOrInt::Right(vector[index]) :
+            ErrorOrInt::Left("'" + std::to_string(index) + "' is out of range");
+    };
+
+    const auto is_zero = [](const auto x) {
+        using SorS = Either<std::string, std::string>;
+        return !(x == 0) ? SorS::Left("false") : SorS::Right("true");
+    };
+    
+    const std::vector<int> array{0, 1, 2, 3, 4, 5};
+
+    start("get(array, 4)");
+    result(get(array, 4)
+        .fmap(log_int)
+        .merge(error, to_string));
+    
+    start("get(array, 100)");
+    result(get(array, 100)
+        .fmap(log_int)
+        .merge(error, to_string));
+
+    start("is_0(0)");
+    result(is_zero(0)
+        .fmap(log)
+        .merge(id, id));
+    start("is_0(10)");
+    result(is_zero(10)
+        .fmap(log)
+        .merge(id, id));
+
+    start("get bind is_zero");
+    result(get(array, 3)
+        .fmap(log_int)
+        .fmap(add(-3))
+        .fmap(log_int)
+        .bind(is_zero)
+        .fmap(log)
+        .merge(id, id));
+
+    start("get bind is_zero take2");
+    result(get(array, 2)
+        .fmap(log_int)
+        .bind(is_zero)
+        .reverse()
+        .fmap(log)
+        .merge(id, id));
+
+    return 0;
+}
+
+
